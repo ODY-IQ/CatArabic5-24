18e51d5b9dc97f10c00b6923ba13719c297f150e
odisho1997 <86355012+odisho1997@users.noreply.github.com>
2023-05-20 23:46:38 +0300

    Update session.py

--- userbot/core/session.py
+++ userbot/core/session.py
@@ -1,17 +1,19 @@
 import sys
 
 from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
-from telethon.sessions import StringSession
 
 from ..Config import Config
+from .StringSession import StringSession
 from .client import CatUserBotClient
+from .logger import logging
 
-__version__ = "5.0.0"
+LOGS = logging.getLogger("CatArabic")
+__version__ = "2.10.6"
 
 loop = None
 
 if Config.STRING_SESSION:
-    session = StringSession(str(Config.STRING_SESSION))
+    session = StringSession(Config.STRING_SESSION, LOGS)
 else:
     session = "catuserbot"
 
@@ -27,10 +29,30 @@ try:
         connection_retries=None,
     )
 except Exception as e:
-    print(f"STRING_SESSION - {e}")
+    print(
+        f"STRING_SESSION WRONG PLZ MAKE A NEW SESSION - {e}\n كود سيشن تيليثون الذي وضعته غير صالح"
+    )
     sys.exit()
 
+try:
+    if Config.STRING_SESSION2:
+        catub2 = JmthonClient(
+            bothseesion(Config.STRING_SESSION2, LOGS),
+            api_id=Config.APP_ID,
+            api_hash=Config.API_HASH,
+            loop=loop,
+            app_version=__version__,
+            connection=ConnectionTcpAbridged,
+            auto_reconnect=True,
+            connection_retries=None,
+        )
+    else:
+        catub2 = None
+except Exception as e:
+    print(f"STRING_SESSION2 - {str(e)}")
+    sys.exit()
 
+    
 catub.tgbot = tgbot = CatUserBotClient(
     session="CatTgbot",
     api_id=Config.APP_ID,
