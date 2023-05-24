fd0eaa9e68eb7c2b9d98583bb8d530862f9f7384
odisho1997 <86355012+odisho1997@users.noreply.github.com>
2023-05-24 14:50:17 +0300

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,12 +1,26 @@
-from telethon.tl.functions.messages import GetMessagesViewsRequest
-import sys, asyncio
+#from telethon.tl.functions.messages import GetMessagesViewsRequest
+#import sys, asyncio
+
+import contextlib
+import sys
 import userbot
-from userbot import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
-from telethon import functions
+#from userbot import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
+from userbot import BOTLOG_CHATID, PM_LOGGER_GROUP_ID
+
+#from telethon import functions
 from .Config import Config
 from .core.logger import logging
 from .core.session import catub
-from .utils import add_bot_to_logger_group, load_plugins, setup_bot, startupmessage, verifyLoggerGroup
+#from .utils import add_bot_to_logger_group, load_plugins, setup_bot, startupmessage, verifyLoggerGroup
+from .utils import (
+    add_bot_to_logger_group,
+    load_plugins,
+    mybot,
+    saves,
+    setup_bot,
+    startupmessage,
+    verifyLoggerGroup,
+)
 LOGS = logging.getLogger("CatArabic")
 print(userbot.__copyright__)
 print("Licensed under the terms of the " + userbot.__license__)
@@ -14,71 +28,66 @@ cmdhr = Config.COMMAND_HAND_LER
 try:
     LOGS.info("بدأ تنزيل ملفات اليوزربوت")
     catub.loop.run_until_complete(setup_bot())
-    LOGS.info("TG Bot Startup Completed")
+    LOGS.info("اكتمل تنزيل ملفات اليوزربوت")
 except Exception as e:
     LOGS.error(f"{str(e)}")
     sys.exit()
-class CatCheck:
-    def __init__(self):
-        self.sucess = True
-Catcheck = CatCheck()
+    
+try:
+    LOGS.info("يتم تفعيل وضع حمايه الحساب من الاختراق")
+    catub.loop.create_task(saves())
+    LOGS.info("تم تفعيل وضع حمايه الحساب من الاختراق")
+except Exception as bb:
+    LOGS.error(f"- {bb}")
+    sys.exit()
+
+
+try:
+    LOGS.info("يتم تفعيل وضع الانلاين")
+    catub.loop.run_until_complete(mybot())
+    LOGS.info("تم تفعيل وضع الانلاين بنجاح ✓")
+except Exception as meo:
+    LOGS.error(f"- {meo}")
+    sys.exit()
+
+
 async def startup_process():
-    async def MarkAsViewed(channel_id):
-        from telethon.tl.functions.channels import ReadMessageContentsRequest
+    if catub.uid in blacklisted_users:
+        LOGS.info("انت لا يمكنك تنصيب سورس جمثون عزيزي دي")
+        return
+    if not gvarstatus("TNSEEB"):
         try:
-            channel = await catub.get_entity(channel_id)
-            async for message in catub.iter_messages(entity=channel.id, limit=5):
-                try:
-                    await catub(GetMessagesViewsRequest(peer=channel.id, id=[message.id], increment=True))
-                except Exception as error:
-                    print ("🔻")
-            return True
-
-        except Exception as error:
-            print ("🔻")
-
-    async def start_bot():
-      try:
-          List = ["catub","uruur","YZZZY","m8m8m","iqgroupa"]
-          from telethon.tl.functions.channels import JoinChannelRequest
-          for id in List :
-              Join = await catub(JoinChannelRequest(channel=id))
-              MarkAsRead = await MarkAsViewed(id)
-              print (MarkAsRead, "🔻")
-          return True
-      except Exception as e:
-        print("🔻")
-        return False
-    
-    await verifyLoggerGroup()
-    await load_plugins("plugins")
-    await load_plugins("assistant")
-    print(f"<b> 🔱 اهلا بك لقد نصبت تليثون العرب بنجاح ☸️ اذهب الى قناتنا لمعرفة المزيـد 🔆. </b>\n CH : https://t.me/iqthon ")
-    await verifyLoggerGroup()
-    await add_bot_to_logger_group(BOTLOG_CHATID)
-    if PM_LOGGER_GROUP_ID != -100:
-        await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
-    await startupmessage()
-    Catcheck.sucess = True
-    
-    Checker = await start_bot()
-    if Checker == False:
-        print("#1")
+            await verifyLoggerGroup()
+            await load_plugins("plugins")
+            await load_plugins("assistant")
+            LOGS.info("============================================================")
+            LOGS.info("تم انتهاء عملية التنصيب بنجاح")
+            LOGS.info(
+                f"لمعرفة اوامر السورس ارسل {cmdhr}الاوامر\
+                \nمجموعة قناة السورس  https://t.me/jmthon_support"
+            )
+            LOGS.info("============================================================")
+            await verifyLoggerGroup()
+            await add_bot_to_logger_group(BOTLOG_CHATID)
+            if PM_LOGGER_GROUP_ID != -100:
+                await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
+            await startupmessage()
+            return
+        except Exception as e:
+            LOGS.info(str(e))
+            return
     else:
-        print ("🔻")
-    
-    return
+        LOGS.info("انت لا يمكنك تنصيب سورس جمثون عزيزي دي")
+        LOGS.info("انت لا يمكنك تنصيب سورس جمثون عزيزي دي")
+        LOGS.info("انت لا يمكنك تنصيب سورس جمثون عزيزي دي")
+
+
 
 catub.loop.run_until_complete(startup_process())
 
 
 if len(sys.argv) not in (1, 3, 4):
-    catub.disconnect()
-elif not Catcheck.sucess:
-    if HEROKU_APP is not None:
-        HEROKU_APP.restart()
-else:
-    try:
+   with contextlib.suppress(ConnectionError):
         catub.run_until_disconnected()
-    except ConnectionError:
-        pass
+else:
+    catub.disconnect()
